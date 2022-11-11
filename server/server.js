bba9149ec879ac1e735dc3dfdcb699f38170e5f9
Ushira Dineth <ushiradineth@gmail.com>
2022-11-11 23:24:25 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -8,13 +8,7 @@ const cors = require('cors')
 
 const app = express()
 
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions))
+app.use(cors())
 
 connectDB()
 
